4d6aca52d6d66ed6a8e0d9687a90c9b1cb8d2027
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-24 19:11:17 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use octocrab;
+use serde_json;
+
+#[tokio::main]
+async fn main() -> Result<(), octocrab::Error> {
+    let github = octocrab::instance();
+    let response: serde_json::Value = github.graphql("query { viewer { login }}").await?;
+    println!("{}", response);
+    Ok(())
 }
