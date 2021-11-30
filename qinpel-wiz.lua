05ab00c49e1e44c536ad81680ab8f6bc9c9b6897
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-30 19:36:59 -0300

    prototype development

--- qinpel-wiz.lua
+++ qinpel-wiz.lua
@@ -1,9 +1,14 @@
 print("Building binary...")
 wiz.cmd("cargo", {"build", "--release"}, ".", true, true)
-local bin_name = "qinpel-wiz" .. wiz.exe_ext
-local bin_origin = "target/release/" .. bin_name
-local bin_destiny = "../../" .. bin_name
-local bin_old = bin_destiny .. "-old"
-wiz.rm(bin_old)
-wiz.mv(bin_destiny, bin_old)
-wiz.cp(bin_origin, bin_destiny)
+local binary_name = "qinpel-wiz" .. wiz.exe_ext
+local binary_origin = "target/release/" .. binary_name
+local binary_destiny = "../../" .. binary_name
+local binary_old = binary_destiny .. "-old"
+wiz.rm(binary_old)
+wiz.mv(binary_destiny, binary_old)
+wiz.cp(binary_origin, binary_destiny)
+local script_name = "qinpel-wiz.sh"
+local script_origin = "./" .. script_name
+local script_destiny = "../../" .. script_name
+wiz.rm(script_destiny)
+wiz.cp(script_origin, script_destiny)
