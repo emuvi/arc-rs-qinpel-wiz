4906f4e96e51eea1f5e8a480974b672480d7cc44
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-30 18:17:03 -0300

    prototype development

--- qinpel-wiz.lua
+++ qinpel-wiz.lua
@@ -1,4 +1,6 @@
 print("Building binaries...")
 wiz.cmd("cargo", {"build", "--release"}, ".", true, true)
 local bin_name = "qinpel-wiz" .. wiz.exe_ext
+print("Binary name = " .. bin_name)
+wiz.mv("../../" .. bin_name, "../../" .. bin_name .. "old")
 wiz.cp("target/release/" .. bin_name, "../../" .. bin_name) 
